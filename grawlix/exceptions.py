76a6d7b0dafc5132c031960718aaac7200909f75
Joakim Holm <mail@joakimholm.xyz>
2023-05-18 22:55:32 +0200

    Improve logging

--- grawlix/exceptions.py
+++ grawlix/exceptions.py
@@ -1,23 +1,30 @@
+from grawlix.logging import print_error_file
+
+ISSUE_URL = "https://github.com/jo1gi/grawlix/issues"
+REPO_URL = "https://github.com/jo1gi/grawlix"
+
 class GrawlixError(Exception):
-    pass
+    error_file: str
+
+    def print_error(self) -> None:
+        print_error_file(
+            self.error_file,
+            repo = REPO_URL,
+            issue = ISSUE_URL,
+        )
+
 
 class DataNotFound(GrawlixError):
-    pass
+    error_file = "data_not_found"
 
 class InvalidUrl(GrawlixError):
-    pass
+    error_file = "invalid_url"
 
 class UnsupportedOutputFormat(GrawlixError):
-    pass
-
-class NoSourceFound(GrawlixError):
-    pass
+    error_file = "unsupported_output_format"
 
 class SourceNotAuthenticated(GrawlixError):
-    pass
-
-class MissingArgument(GrawlixError):
-    pass
+    error_file = "source_not_authenticated"
 
 class ThrottleError(GrawlixError):
-    pass
+    error_file = "throttle"
